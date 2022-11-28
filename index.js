0ac122f3e3cdae976c2c70d31bc8a683ddce0007
Token Kim <tokenkim92@gmail.com>
2022-11-28 23:40:08 +0100

    feature: add background music

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 import './lib/typefill.min.js';
 import './lib/typeparticle.min.js';
 
-const title = new TypeFill('title', 5000);
+const title = new TypeFill('title', 6000);
 const implementer = new TypeParticle('implementer', 5);
-
 title.setHandleStop(implementer.start);
 
+const playButton = document.querySelector('#play-btn');
+const backgroundAudio = new Audio('./resource/backgroundMusic.mp3');
+const playIcon = playButton.querySelector('.fa-solid');
+let played = false;
+
+playButton.addEventListener('click', () =>
+  played ? pauseBackgroundAudio() : playBackgroundAudio()
+);
+
+backgroundAudio.addEventListener(
+  'ended',
+  () => played && pauseBackgroundAudio()
+);
+
 title.start();
+
+const playBackgroundAudio = () => {
+  played = true;
+
+  playIcon.classList.add('fa-volume-high');
+  playIcon.classList.remove('fa-volume-xmark');
+  backgroundAudio.play();
+};
+
+const pauseBackgroundAudio = () => {
+  played = false;
+
+  playIcon.classList.add('fa-volume-xmark');
+  playIcon.classList.remove('fa-volume-high');
+  backgroundAudio.pause();
+  backgroundAudio.currentTime = 0;
+};
